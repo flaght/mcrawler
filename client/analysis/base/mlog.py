110d70f96364df94c9007d98f9919bc1fab3b32b
kerry <kerry@miglab.com>
2016-11-15 20:45:26 +0800

    *修改日志类

--- client/analysis/base/mlog.py
+++ client/analysis/base/mlog.py
@@ -9,32 +9,34 @@ Created on 2014年6月6日
 '''
 
 class MLog(object):
-    '''
+    """
     classdocs
-    '''
+    """
 
 
-    def __init__(self):
-        '''
+    def __init__(self,name = "logging"):
+        """
         Constructor
-        '''
-        filename = 'cralwer_'+datetime.datetime.now().strftime('%b_%d_%y-%H')+'.log'
-        format_str = 'pid :%(process)d tid :%(thread)d  %(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s'
-        logging.basicConfig(level=logging.ERROR,
-                format=format_str,
-                datefmt='%a, %d %b %Y %H:%M:%S',
-                filename= filename,
-                filemode='a')
-        ##控制台输出
-        console = logging.StreamHandler()  
+        """
+        filename = name + datetime.datetime.now().strftime('%b_%d')+'.log'
+        #[Tue Nov 15 11:35:53 2016] [notice] Apache/2.2.15 (Unix) DAV/2 PHP/5.3.3 mod_ssl/2.2.15 OpenSSL/1.0.1e-fips configured -- resuming normal operations
+        format_str = "[%(process)d %(thread)d][%(asctime)s][%(filename)s line:%(lineno)d][%(levelname)s] %(message)s"
+        # define a Handler which writes INFO messages or higher to the sys.stderr
+        logging.basicConfig(level=logging.DEBUG,
+                            format=format_str,
+                            datefmt='%m-%d %H:%M',
+                            filename=filename,
+                            filemode='a')
+        console = logging.StreamHandler()
         console.setLevel(logging.INFO)
-        console.setLevel(logging.DEBUG) 
-        formatter = logging.Formatter(format_str)  
-        console.setFormatter(formatter)  
-        # 将定义好的console日志handler添加到root logger  
+        formatter = logging.Formatter(format_str)
+        console.setFormatter(formatter)
+        # 将定义好的console日志handler添加到root logger
         logging.getLogger('').addHandler(console)
 
+
     def log(self):
+        #logging.error()
         return logging
 
 mlog = MLog()
